c1be7c0524d984465d51dedafb45b8b466bce83c
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-09-09 10:33:49 +0000

    [Start3-2]

---
+++ db/migrate/20220907125801_create_lists.rb
@@ -0,0 +1,6 @@
+ create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
